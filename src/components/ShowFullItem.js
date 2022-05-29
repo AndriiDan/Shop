3b356ed484ca3bbd060fa509b478d2fbf8fd7753
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-05-29 16:01:26 +0300

    8. Стилі для модального вікна.

--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -4,12 +4,15 @@ export class ShowFullItem extends Component {
     render() {
         return (
             <div className='full-item'>
-                <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} />
-                <h2>{this.props.item.title}</h2>
-                <p>{this.props.item.desc}</p>
-                <b>{this.props.item.price} грн.</b>
-                {/* при onClick елемент item буде додаватися в корзину (до масива oders в App) */}
-                <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+                <div>
+                    {/* img повинні знаходитися в папці publik/img */}
+                    <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} />
+                    <h2>{this.props.item.title}</h2>
+                    <p>{this.props.item.desc}</p>
+                    <b>{this.props.item.price} грн.</b>
+                    {/* при onClick елемент item буде додаватися в корзину (до масива oders в App) */}
+                    <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+                </div>
             </div>
         )
     }
