c4d75840bc48633f4e899e6c632ab7d2159f2da8
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-06-28 00:38:21 +0300

    NavLink.

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 // import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 // к-та навігації по застосунку
 const Navbar = () => {
     return (
         <ul className={classes.nav}>
-            <li><a href="/flowers">Квіти</a></li>
-            <li><a href="/aboutUs">Про нас</a></li>
-            <li><a href="/contacts">Контакти</a></li>
-            <li><a href="/office">Кабінет</a></li>
+            <li className={classes.item}><NavLink to="/flowers" className={navData => navData.isActive ? classes.active : classes.item}>Квіти</NavLink></li>
+            <li className={classes.item}><NavLink to="/aboutUs" className={navData => navData.isActive ? classes.active : classes.item}>Про нас</NavLink></li>
+            <li className={classes.item}><NavLink to="/contacts" className={navData => navData.isActive ? classes.active : classes.item}>Контакти</NavLink></li>
+            <li className={classes.item}><NavLink to="/office" className={navData => navData.isActive ? classes.active : classes.item}>Кабінет</NavLink></li>
         </ul>
     )
 }
