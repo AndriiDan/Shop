ac85cd2bc5f918af0b9d82cf875fd9a208abeb2b
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-06-29 19:59:39 +0300

    Рефакторінг Reviews.

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,27 +3,33 @@ import AddReview from './AddReview/AddReview';
 import Review from './Review/Review';
 import classes from './Reviews.module.css';
 
-let reviewsData = [
-    { id: 1, review: "Дякую, молоці!", likesCount: 5 },
-    { id: 2, review: "Дуже гарні квіти.", likesCount: 10 },
-    { id: 3, review: "Замовлення доставили швидко. Дякую.", likesCount: 7 }
-]
+
 
 // к-та відгуків про магазин
 const Reviews = () => {
+
+    // масив даних (імітація сервера)
+    let reviewsData = [
+        { id: 1, review: "Дякую, молоці!", likesCount: 5 },
+        { id: 2, review: "Дуже гарні квіти.", likesCount: 10 },
+        { id: 3, review: "Замовлення доставили швидко. Дякую.", likesCount: 7 }
+    ]
+
+    // новий масив із компонент Review-відгуків
+    let reviewsElements = reviewsData.map((review) => {
+        let rev = <Review review={review.review} likesCount={review.likesCount} />;
+        return (
+            // додатково <div> для стилів, щоб кожний відгук був з нового рядка
+            <div>{rev}</div>
+        )
+    });
+
     return (
         <div>
             <h1>Відгуки про нас:</h1>
             <div className={classes.reviews}>
-                <div>
-                    <Review review={reviewsData[0].review} likesCount={reviewsData[0].likesCount} />
-                </div>
-                <div>
-                    <Review review={reviewsData[1].review} likesCount={reviewsData[1].likesCount} />
-                </div>
-                <div>
-                    <Review review={reviewsData[2].review} likesCount={reviewsData[2].likesCount} />
-                </div>
+                {/* список відгуків */}
+                <div>{reviewsElements}</div>
             </div>
             <AddReview />
         </div>
