4f0fd8edac1627bc6ef73fd8f0aefdd6c1cb100d
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-08-14 23:52:12 +0300

    Скорочення коду (mapDispatchToProps) для ReviewsContainer.

--- src/components/Reviews/ReviewsContainer.jsx
+++ src/components/Reviews/ReviewsContainer.jsx
@@ -10,28 +10,13 @@ const mapStateToProps = (state) => {
     }
 };
 
-// методи (ф-ції), які передадуться в <Reviews />
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addReview: () => {
-            // callback для компоненти AddReview - "додати відгук"
-            // actionCreator - додавання відгуку
-            let action = addNewReviewActionCreator();
-            // виконання ф-ції
-            dispatch(action);
-        },
-        reviewChange: (text) => {
-            // callback для компоненти AddReview - обробник подій для textarea (посимвольне оновлення через оновлення state)
-            // actionCreator - значення з textarea
-            let action = updateNewReviewActionCreator(text);
-            // оновлення тексту в textarea через оновлення state
-            dispatch(action);
-        }
-    }
-};
-
 // к-та відгуків про магазин
 // контейнерна компонента над презинтаційною (Reviews) компонентою
-const ReviewsContainer = connect(mapStateToProps, mapDispatchToProps)(Reviews);
+const ReviewsContainer = connect(mapStateToProps,
+    { addReview: addNewReviewActionCreator, reviewChange: updateNewReviewActionCreator }
+)(Reviews);
 
 export default ReviewsContainer;
+
+// addReview - callback для компоненти AddReview - "додати відгук"
+// reviewChange - callback для компоненти AddReview - обробник подій для textarea (посимвольне оновлення через оновлення state)
