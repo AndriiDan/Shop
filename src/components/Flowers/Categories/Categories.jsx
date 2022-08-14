689260ddc9374136d007701a03aaac2d7db5fbbc
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-08-15 00:07:36 +0300

    Скорочення коду (mapDispatchToProps) для FlowersContainer.

--- src/components/Flowers/Categories/Categories.jsx
+++ src/components/Flowers/Categories/Categories.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { chooseCategoryActionCreator } from '../../../redux/flowers-reducer';
 import classes from './Categories.module.css';
 
 // к-та для відображення категорій квітів
@@ -7,8 +6,6 @@ const Categories = (props) => {
     return (
         <div className={classes.categories}>
             {props.categories.map(el => (
-                // метод dispatch(chooseCategoryActionCreator(el.key)) вибрати категорію товару; + в метод передається key для категорії 
-                // <div key={el.key} onClick={() => props.dispatch(chooseCategoryActionCreator(el.key))}>{el.name}</div>
                 <div key={el.key} onClick={() => {
                     // callback з FlowersContainer - ф-ція (через dispatch(action)) - вибір категорії; + в callback передається (el.key) = (category)
                     props.chooseCategory(el.key)
--- src/components/Flowers/FlowersContainer.jsx
+++ src/components/Flowers/FlowersContainer.jsx
@@ -12,34 +12,41 @@ const mapStateToProps = (state) => {
     }
 };
 
-// методи (ф-ції), які передадуться в <Flowers />
-const mapDispatchToProps = (dispatch) => {
-    return {
-        chooseCategory: (categories) => {
-            // callback для компоненти Categories
-            // actionCreator - метод для вибору категорії dispatch(chooseCategoryActionCreator(el.key); + в метод передається (el.key) = (categories)
-            let action = chooseCategoryActionCreator(categories);
-            // виконання ф-ції вибору категорії
-            dispatch(action);
-        },
-        addToOrder: (item) => {
-            // callback для компоненти Item
-            // actionCreator - метод для додавання товару в корзину покупок dispatch(addToOrderActionCreator(item); + в метод передається item
-            let action = addToOrderActionCreator(item);
-            // виконання ф-ції додавання товару в корзину
-            dispatch(action);
-        },
-        showItem: (item) => {
-            // callback для компоненти Item
-            // метод dispatch(onShowItemActionCreator(props.item)) показати товар у модальному вікні; + в метод передається item */}
-            let action = onShowItemActionCreator(item);
-            // виконання ф-ції показати товар у модальному вікні
-            dispatch(action);
-        }
-    }
-};
+// // методи (ф-ції), які передадуться в <Flowers />
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         chooseCategory: (categories) => {
+//             // callback для компоненти Categories
+//             // actionCreator - метод для вибору категорії dispatch(chooseCategoryActionCreator(el.key); + в метод передається (el.key) = (categories)
+//             let action = chooseCategoryActionCreator(categories);
+//             // виконання ф-ції вибору категорії
+//             dispatch(action);
+//         },
+//         addToOrder: (item) => {
+//             // callback для компоненти Item
+//             // actionCreator - метод для додавання товару в корзину покупок dispatch(addToOrderActionCreator(item); + в метод передається item
+//             let action = addToOrderActionCreator(item);
+//             // виконання ф-ції додавання товару в корзину
+//             dispatch(action);
+//         },
+//         showItem: (item) => {
+//             // callback для компоненти Item
+//             // метод dispatch(onShowItemActionCreator(props.item)) показати товар у модальному вікні; + в метод передається item */}
+//             let action = onShowItemActionCreator(item);
+//             // виконання ф-ції показати товар у модальному вікні
+//             dispatch(action);
+//         }
+//     }
+// };
 
 // контейнерна компонента над презинтаційною (Flowers) компонентою
-const FlowersContainer = connect(mapStateToProps, mapDispatchToProps)(Flowers);
+const FlowersContainer = connect(mapStateToProps,
+    //  mapDispatchToProps
+    { chooseCategory: chooseCategoryActionCreator, addToOrder: addToOrderActionCreator, showItem: onShowItemActionCreator }
+)(Flowers);
 
 export default FlowersContainer;
+
+// chooseCategory - метод для вибору категорії dispatch(chooseCategoryActionCreator(el.key); + в метод передається (el.key) = (categories)
+// addToOrder - метод для додавання товару в корзину покупок dispatch(addToOrderActionCreator(item); + в метод передається item
+// showItem - // метод dispatch(onShowItemActionCreator(props.item)) показати товар у модальному вікні; + в метод передається item */}
